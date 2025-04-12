bd86664574208f233c82374ab41cf32c870eb5ba
Viet Pham <viet@webonyx.com>
2025-04-12 20:44:38 +0700

    update cmd: not require config file

--- cmd/fyve/update.go
+++ cmd/fyve/update.go
@@ -3,7 +3,6 @@ package fyve
 import (
 	"context"
 	"fmt"
-	"github.com/fyve-labs/fyve-cli/pkg/config"
 	"github.com/fyve-labs/fyve-cli/pkg/docker"
 	"github.com/spf13/cobra"
 	"time"
@@ -16,7 +15,6 @@ func init() {
 func UpdateCmd() *cobra.Command {
 	var (
 		imageTag   string
-		configFile string
 		dockerHost string
 	)
 
@@ -25,21 +23,17 @@ func UpdateCmd() *cobra.Command {
 		Short: "Update Cosmos application",
 		Args:  cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// Load configuration
-			cfg, err := config.Load(configFile)
-			if err != nil {
-				return fmt.Errorf("failed to load config: %w", err)
-			}
-
-			// Override app name if provided via args
-			appName := cfg.App
+			var appName string
 			if len(args) > 0 && args[0] != "" {
 				appName = args[0]
-				cfg.OverrideAppName(appName)
 			}
 
 			if appName == "" {
-				return fmt.Errorf("app name must be specified in config or as an argument")
+				return fmt.Errorf("app name is required")
+			}
+
+			if imageTag == "" {
+				return fmt.Errorf("image tag is required")
 			}
 
 			ctx, cancel := context.WithTimeout(context.Background(), time.Minute*10)
@@ -59,7 +53,6 @@ func UpdateCmd() *cobra.Command {
 
 	// Add flags
 	cmd.Flags().StringVarP(&imageTag, "tag", "t", "", "New image tag")
-	cmd.Flags().StringVarP(&configFile, "config", "c", "fyve.yaml", "Path to configuration file")
 	cmd.Flags().StringVarP(&dockerHost, "docker-host", "d", "tcp://10.100.26.239:2375", "Remote Docker host URL")
 
 	return cmd
